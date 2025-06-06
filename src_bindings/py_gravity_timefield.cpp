f20d388fafd3c37a96b9e8a832099f208c1a8f44
Omar Iskandarani <omar.s.iskandarani@gmail.com>
2025-06-06 18:03:25 +0200

    Align bindings with class-based API

--- src_bindings/py_gravity_timefield.cpp
+++ src_bindings/py_gravity_timefield.cpp
@@ -9,13 +9,13 @@
 namespace py = pybind11;
 
 void bind_gravity_timefield(py::module_& m) {
-	m.def("compute_gravitational_potential", &vam::compute_gravitational_potential,
+        m.def("compute_gravitational_potential", &vam::GravityTimeField::compute_gravitational_potential,
 		  py::arg("positions"), py::arg("vorticity"), py::arg("epsilon") = 0.1,
 		  R"pbdoc(
             Compute Æther gravitational potential field from vorticity.
         )pbdoc");
 
-	m.def("compute_time_dilation_map", &vam::compute_time_dilation_map,
+        m.def("compute_time_dilation_map", &vam::GravityTimeField::compute_time_dilation_map,
 		  py::arg("tangents"), py::arg("C_e"),
 		  R"pbdoc(
             Compute local time dilation factor map from tangential velocities.
